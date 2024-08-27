fa79c74672bb6d828bfb53043d1f4290b2c59805
Eugene-Khonich <ehonich@gmail.com>
2024-08-27 20:52:07 +0300

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,18 @@
-const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
 
-export const fetchPhotos = searchValue => {
-  const urlParams = new URLSearchParams({
-    key: '45505147-33e194e8689097e70f28421cd',
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  return fetch(`${BASE_URL}?${urlParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export function fetchPhotos(searchValue, page) {
+  const axiosOptions = {
+    params: {
+      key: '45505147-33e194e8689097e70f28421cd',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      q: searchValue,
+      page: page,
+      per_page: 15,
+    },
+  };
+  return axios.get('', axiosOptions);
+}
--- src/main.js
+++ src/main.js
@@ -6,48 +6,94 @@ import { greateCards } from './js/render-functions';
 const form = document.querySelector('.form');
 const galleryList = document.querySelector('.gallery-list');
 const loader = document.querySelector('.loader');
+const loadMore = document.querySelector('.load-more');
 const lightBox = new SimpleLightbox('.gallery-list a', {
   captionsData: 'alt',
   captionDelay: 250,
   overlayOpacity: 0.8,
 });
 
-const onSearchSubmit = e => {
+let value = '';
+let page = 1;
+let cardHeight = 0;
+let totalPages = 0;
+let perPage = 15;
+
+const onSearchSubmit = async e => {
   e.preventDefault();
-  loader.classList.remove('hidden');
-  const value = e.target.elements.inputField.value.toLowerCase().trim();
+  value = e.target.elements.inputField.value.toLowerCase().trim();
+  page = 1;
   if (!value) {
     iziToast.error({
       title: 'Error',
       message: 'You need to type something on field!',
       position: 'topRight',
     });
-    loader.classList.add('hidden');
     return;
   }
-  fetchPhotos(value)
-    .then(img => {
-      if (img.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-        loader.classList.add('hidden');
-        return;
-      } else {
-        greateCards(img.hits);
-        lightBox.refresh();
-        loader.classList.add('hidden');
-      }
-    })
-    .catch(err => {
+  loader.classList.remove('hidden');
+  try {
+    const response = await fetchPhotos(value, page);
+    if (response.data.total === 0) {
       iziToast.error({
-        message: `There is an Error ${err}. Try again!`,
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
       });
+      loader.classList.add('hidden');
+      galleryList.innerHTML = '';
+      form.reset();
+      return;
+    } else {
+      greateCards(response.data.hits);
+      lightBox.refresh();
+      loader.classList.add('hidden');
+      loadMore.classList.remove('hidden');
+    }
+  } catch (err) {
+    iziToast.error({
+      message: `There is an Error ${err}. Try again!`,
+      position: 'topRight',
     });
-  form.reset();
+  }
+};
+
+const onLoadMoreClick = async e => {
+  try {
+    page += 1;
+    loadMore.classList.add('hidden');
+    loader.classList.remove('hidden');
+    const response = await fetchPhotos(value, page);
+    greateCards(response.data.hits);
+    totalPages = response.data.totalHits;
+    console.log(response);
+    console.log(page);
+    console.log(totalPages);
+    if (page * perPage >= totalPages) {
+      iziToast.error({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+      loadMore.classList.add('hidden');
+      loader.classList.add('hidden');
+      return;
+    }
+    const galleryCard = galleryList.querySelector('.gallery-item');
+    cardHeight = galleryCard.getBoundingClientRect().height;
+    scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    loader.classList.add('hidden');
+    loadMore.classList.remove('hidden');
+    lightBox.refresh();
+  } catch (err) {
+    iziToast.error({
+      message: `There is an Error ${err}. Try again!`,
+      position: 'topRight',
+    });
+  }
 };
 
 form.addEventListener('submit', onSearchSubmit);
+loadMore.addEventListener('click', onLoadMoreClick);
